e5b0dc454717a34acc0c2d1cf29d5203068963c8
NofalSher <nofalsher7@gmail.com>
2025-06-24 19:31:17 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-from typing import Annotated
-def double(x: int) -> int:
+from typing import Annotated,get_type_hints
+def double(x: Annotated[int,(0,100)]) -> int:
+    type_hints = get_type_hints(double,include_extras=True)
+    print(type_hints)  # Output: {'x': <class 'int'>, 'return': <class 'int'>}
     return x * 2
 
 result = double(4)
-print(result)  # Output: 8
+print(result)  # Output: 8``
